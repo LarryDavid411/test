ec125cc5489bb36f0a5de7e58d54f10ae25cd8e5
Adam Socki <adamsocki@Adams-MBP.attlocal.net>
2020-07-15 18:44:57 -0500

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Adam Socki on 7/15/20.
+//  Copyright © 2020 Adam Socki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
